81e8f1373038c22e79a25ddb082240d0a603f81d
sameer shaikh <sam007.bagwan@gmail.com>
2020-12-16 10:51:50 +0530

    first commit

---
+++ Selenium POC/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Selenium_POC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            IWebDriver webDriver = new ChromeDriver();
+            webDriver.Navigate().GoToUrl("http://www.google.com");
+            webDriver.FindElement(By.Name("q")).SendKeys("Learn Selenuim");
+            webDriver.TakeScreenshot();
+            //webDriver.Quit();
+            //webDriver.Close();
+        }
+    }
+}
